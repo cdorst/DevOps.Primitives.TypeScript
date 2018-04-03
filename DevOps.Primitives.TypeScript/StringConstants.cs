19fb45b91ebf1232c53834f7f96e506158805aa2
Christopher Dorst <chris@dorst.email>
2018-04-03 10:17:10 -0500

    Add StringConstants internal helper

---
+++ DevOps.Primitives.TypeScript/StringConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace DevOps.Primitives.TypeScript
+{
+    internal static class StringConstants
+    {
+        public const string CloseCurlyBrace = "}";
+        public const string Indent = "    ";
+        public const string NewLine = "\r\n";
+    }
+}
