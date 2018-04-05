2a20f175fb760d15e11456ed51b85313236d7bf8
Christopher Dorst <chris@dorst.email>
2018-04-05 06:24:51 -0500

    Add BaseListKind class/interface discriminator

---
+++ DevOps.Primitives.TypeScript/BaseListKind.cs
@@ -0,0 +1,12 @@
+﻿using Common.EnumStringValues;
+
+namespace DevOps.Primitives.TypeScript
+{
+    public enum BaseListKind : byte
+    {
+        [EnumStringValue("extends")]
+        Extends,
+        [EnumStringValue("implements")]
+        Implements
+    }
+}
