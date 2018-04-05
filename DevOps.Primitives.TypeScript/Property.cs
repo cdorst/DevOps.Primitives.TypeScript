eec97ccb7be4f094b42e8df62173c7f2326c9e64
Christopher Dorst <chris@dorst.email>
2018-04-05 06:33:20 -0500

    Implement GetPropertySyntax

--- DevOps.Primitives.TypeScript/Property.cs
+++ DevOps.Primitives.TypeScript/Property.cs
@@ -1,7 +1,10 @@
 ﻿using Common.EntityFrameworkServices;
+using Common.EnumStringValues;
 using ProtoBuf;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
 
 namespace DevOps.Primitives.TypeScript
 {
@@ -13,26 +16,32 @@ namespace DevOps.Primitives.TypeScript
         public Property(
             Identifier identifier,
             Identifier type,
-            AccessorList accessorList,
-            ModifierList modifierList = null,
-            DocumentationCommentList documentationCommentList = null,
-            DecoratorList attributeListCollection = null)
+            DocumentationComment documentationComment,
+            AccessModifiers? accessModifier = null,
+            bool isStatic = false,
+            bool isReadonly = false,
+            DecoratorList decoratorList = null,
+            Expression defaultValue = null)
         {
             Identifier = identifier;
             Type = type;
-            AccessorList = accessorList;
-            ModifierList = modifierList;
-            DocumentationCommentList = documentationCommentList;
-            AttributeListCollection = attributeListCollection;
+            DocumentationComment = documentationComment;
+            AccessModifier = accessModifier;
+            IsStatic = isStatic;
+            IsReadonly = isReadonly;
+            DecoratorList = decoratorList;
+            DefaultValue = defaultValue;
         }
         public Property(
             string identifier,
             string type,
-            AccessorList accessorList,
-            ModifierList modifierList = null,
-            DocumentationCommentList documentationCommentList = null,
-            DecoratorList attributeListCollection = null)
-            : this(new Identifier(identifier), new Identifier(type), accessorList, modifierList, documentationCommentList, attributeListCollection)
+            string comment,
+            AccessModifiers? accessModifier = null,
+            bool isStatic = false,
+            bool isReadonly = false,
+            DecoratorList decoratorList = null,
+            Expression defaultValue = null)
+            : this(new Identifier(identifier), new Identifier(type), new DocumentationComment(comment), accessModifier, isStatic, isReadonly, decoratorList, defaultValue)
         {
         }
 
@@ -41,33 +50,56 @@ namespace DevOps.Primitives.TypeScript
         public int PropertyId { get; set; }
 
         [ProtoMember(2)]
-        public AccessorList AccessorList { get; set; }
+        public AccessModifiers? AccessModifier { get; set; }
+
         [ProtoMember(3)]
-        public int AccessorListId { get; set; }
+        public bool IsStatic { get; set; }
 
         [ProtoMember(4)]
-        public DecoratorList AttributeListCollection { get; set; }
+        public bool IsReadonly { get; set; }
+
         [ProtoMember(5)]
-        public int? AttributeListCollectionId { get; set; }
-
+        public DecoratorList DecoratorList { get; set; }
         [ProtoMember(6)]
-        public DocumentationCommentList DocumentationCommentList { get; set; }
-        [ProtoMember(7)]
-        public int? DocumentationCommentListId { get; set; }
+        public int? DecoratorListId { get; set; }
 
+        [ProtoMember(7)]
+        public Expression DefaultValue { get; set; }
         [ProtoMember(8)]
-        public Identifier Identifier { get; set; }
+        public int? DefaultValueId { get; set; }
+
         [ProtoMember(9)]
+        public DocumentationComment DocumentationComment { get; set; }
+        [ProtoMember(10)]
+        public int DocumentationCommentId { get; set; }
+
+        [ProtoMember(11)]
+        public Identifier Identifier { get; set; }
+        [ProtoMember(12)]
         public int IdentifierId { get; set; }
 
-        [ProtoMember(10)]
-        public ModifierList ModifierList { get; set; }
-        [ProtoMember(11)]
-        public byte? ModifierListId { get; set; }
-
-        [ProtoMember(12)]
-        public Identifier Type { get; set; }
         [ProtoMember(13)]
+        public Identifier Type { get; set; }
+        [ProtoMember(14)]
         public int TypeId { get; set; }
+
+        public string GetPropertySyntax()
+        {
+            var stringBuilder = new StringBuilder().Append($"/** {DocumentationComment} */");
+            var decorators = DecoratorList?.GetRecords().Select(decorator => decorator.GetDecoratorSyntax());
+            foreach (var decorator in decorators ?? new string[] { })
+                stringBuilder.AppendLine(decorator);
+            return stringBuilder
+                .AppendLine($"{GetModifiers()}{Identifier}: {Type}{DefaultValue?.GetDefaultValueAssignmentSyntax()}")
+                .ToString();
+        }
+
+        private string GetModifiers()
+        {
+            var modifiers = AccessModifier == null ? string.Empty : $"{AccessModifier.GetStringValue()} ";
+            if (IsStatic) modifiers = $"{modifiers}static ";
+            if (IsReadonly) modifiers = $"{modifiers}readonly ";
+            return modifiers;
+        }
     }
 }
