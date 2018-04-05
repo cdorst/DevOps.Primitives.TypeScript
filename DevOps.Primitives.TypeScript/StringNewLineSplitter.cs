2b692ab2de98dc9663729396bbed57150c263f6d
Christopher Dorst <chris@dorst.email>
2018-04-05 06:23:00 -0500

    Add string.Split(NewLine) helper

---
+++ DevOps.Primitives.TypeScript/StringNewLineSplitter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static DevOps.Primitives.TypeScript.StringConstants;
+
+namespace DevOps.Primitives.TypeScript
+{
+    internal static class StringNewLineSplitter
+    {
+        public static string[] SplitLines(this string instance)
+            => instance.Split(new[] { NewLine }, StringSplitOptions.None);
+    }
+}
