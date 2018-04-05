6ac3cca090ba25848e2bb640b1b228134d68b53f
Christopher Dorst <chris@dorst.email>
2018-04-05 06:22:29 -0500

    Add access modifier enum

---
+++ DevOps.Primitives.TypeScript/AccessModifiers.cs
@@ -0,0 +1,14 @@
+﻿using Common.EnumStringValues;
+
+namespace DevOps.Primitives.TypeScript
+{
+    public enum AccessModifiers : byte
+    {
+        [EnumStringValue("private")]
+        Private,
+        [EnumStringValue("protected")]
+        Protected,
+        [EnumStringValue("public")]
+        Public
+    }
+}
