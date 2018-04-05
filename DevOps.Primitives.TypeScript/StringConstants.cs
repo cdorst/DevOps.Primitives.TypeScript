b581713d2fc616b21e2f2389936e3c450147f039
Christopher Dorst <chris@dorst.email>
2018-04-05 06:26:51 -0500

    Add StringConstants

--- DevOps.Primitives.TypeScript/StringConstants.cs
+++ DevOps.Primitives.TypeScript/StringConstants.cs
@@ -3,7 +3,14 @@
     internal static class StringConstants
     {
         public const string CloseCurlyBrace = "}";
+        public const string CloseJsDoc = " */";
+        public const string CloseParenthesis = ")";
+        public const string CommaSpace = ", ";
         public const string Indent = "    ";
         public const string NewLine = "\r\n";
+        public const string OpenCurlyBrace = "{";
+        public const string OpenJsDoc = "/**";
+        public const string OpenParenthesis = "(";
+        public const string Space = " ";
     }
 }
