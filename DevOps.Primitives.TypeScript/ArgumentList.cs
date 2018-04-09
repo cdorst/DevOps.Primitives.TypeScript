13f158e0c4dbd26f104afbd9c6e058bc22e7c3bc
Christopher Dorst <chris@dorst.email>
2018-04-08 19:04:25 -0500

    Remove using

--- DevOps.Primitives.TypeScript/ArgumentList.cs
+++ DevOps.Primitives.TypeScript/ArgumentList.cs
@@ -5,7 +5,6 @@ using ProtoBuf;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 
 namespace DevOps.Primitives.TypeScript
 {
