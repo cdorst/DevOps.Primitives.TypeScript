13f764a91b6b72ce4decb7f6495ced8a276a80ae
Christopher Dorst <chris@dorst.email>
2018-04-10 20:42:00 -0500

    Refactor .ToString()

--- DevOps.Primitives.TypeScript/TsConfigInfo.cs
+++ DevOps.Primitives.TypeScript/TsConfigInfo.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using static Common.Functions.CheckNullableEnumerationForAnyElements.NullableEnumerationAny;
+using static DevOps.Primitives.TypeScript.JsonStringBuilderHelper;
+using static DevOps.Primitives.TypeScript.StringConstants;
 
 namespace DevOps.Primitives.TypeScript
 {
     public class TsConfigInfo
     {
+        private const string CompilerOptions = nameof(CompilerOptions);
+
         public TsConfigInfo() { }
         public TsConfigInfo(TsConfigTarget target, TsConfigModule module, IEnumerable<TsConfigLib> lib = null, bool allowJs = false, bool checkJs = false, TsConfigJsx? jsx = null, bool declaration = false, bool sourceMap = false, string outFile = null, string outDir = null, string rootDir = null, bool removeComments = false, bool noEmit = false, bool importHelpers = false, bool downLevelIteration = false, bool isolatedModules = false, bool strict = false, bool noImplicitAny = false, bool strictNullChecks = false, bool strictFunctionsTypes = false, bool strictPropertyInitialization = false, bool noImplicitThis = false, bool alwaysStrict = false, bool noUnusedLocals = false, bool noUnusedParameters = false, bool noImplicitReturns = false, bool noFallthroughCasesInSwitch = false, TsConfigModuleResolution? moduleResolution = null, string baseUrl = null, IDictionary<string, IEnumerable<string>> paths = null, IEnumerable<string> rootDirs = null, IEnumerable<string> typeRoots = null, IEnumerable<string> types = null, bool allowSyntheticDefaultImports = false, bool esModuleInterop = false, bool preserveSymlinks = false, string sourceRoot = null, string mapRoot = null, bool inlineSourceMap = false, bool inlineSources = false, bool experimentalDecorators = false, bool emitDecoratorMetadata = false, IEnumerable<string> includePaths = null)
         {
@@ -53,7 +56,7 @@ namespace DevOps.Primitives.TypeScript
             InlineSources = inlineSources;
             ExperimentalDecorators = experimentalDecorators;
             EmitDecoratorMetadata = emitDecoratorMetadata;
-            IncludePaths = includePaths;
+            Include = includePaths;
         }
 
         public TsConfigTarget Target { get; set; }
@@ -98,123 +101,60 @@ namespace DevOps.Primitives.TypeScript
         public bool InlineSources { get; set; }
         public bool ExperimentalDecorators { get; set; }
         public bool EmitDecoratorMetadata { get; set; }
-        public IEnumerable<string> IncludePaths { get; set; }
+        public IEnumerable<string> Include { get; set; }
 
         public override string ToString()
-        {
-            var stringBuilder = new StringBuilder()
-                .Append("{")
-                .AppendLine("  \"compilerOptions\": {")
-                .AppendLine($"    \"target\": \"{Target.GetStringValue()}\",")
-                .AppendLine($"    \"module\": \"{Module.GetStringValue()}\"");
-            if (Any(Lib))
-            {
-                stringBuilder.Append(",").AppendLine("    \"lib\": [");
-                var count = Lib.Count();
-                for (int i = 0; i < count; i++)
-                {
-                    stringBuilder.AppendLine($"      \"{Lib.ElementAt(i).GetStringValue()}\"");
-                    if (i != count - 1) stringBuilder.Append(",");
-                }
-                stringBuilder.AppendLine("    ]");
-            }
-            if (AllowJs) stringBuilder.Append(",").AppendLine("    \"allowJs\": true");
-            if (CheckJs) stringBuilder.Append(",").AppendLine("    \"checkJs\": true");
-            if (Jsx != null) stringBuilder.Append(",").AppendLine($"    \"jsx\": \"{Jsx.GetStringValue()}\"");
-            if (Declaration) stringBuilder.Append(",").AppendLine("    \"declaration\": true");
-            if (SourceMap) stringBuilder.Append(",").AppendLine("    \"sourceMap\": true");
-            if (!string.IsNullOrEmpty(OutFile)) stringBuilder.Append(",").AppendLine($"    \"outFile\": \"{OutFile}\"");
-            if (!string.IsNullOrEmpty(OutDir)) stringBuilder.Append(",").AppendLine($"    \"outDir\": \"{OutDir}\"");
-            if (!string.IsNullOrEmpty(RootDir)) stringBuilder.Append(",").AppendLine($"    \"rootDir\": \"{RootDir}\"");
-            if (RemoveComments) stringBuilder.Append(",").AppendLine("    \"removeComments\": true");
-            if (NoEmit) stringBuilder.Append(",").AppendLine("    \"noEmit\": true");
-            if (ImportHelpers) stringBuilder.Append(",").AppendLine("    \"importHelpers\": true");
-            if (DownLevelIteration) stringBuilder.Append(",").AppendLine("    \"downlevelIteration\": true");
-            if (IsolatedModules) stringBuilder.Append(",").AppendLine("    \"isolatedModules\": true");
-            if (Strict) stringBuilder.Append(",").AppendLine("    \"strict\": true");
-            if (NoImplicitAny) stringBuilder.Append(",").AppendLine("    \"noImplicitAny\": true");
-            if (StrictNullChecks) stringBuilder.Append(",").AppendLine("    \"strictNullChecks\": true");
-            if (StrictFunctionsTypes) stringBuilder.Append(",").AppendLine("    \"strictFunctionTypes\": true");
-            if (StrictPropertyInitialization) stringBuilder.Append(",").AppendLine("    \"strictPropertyInitialization\": true");
-            if (NoImplicitThis) stringBuilder.Append(",").AppendLine("    \"noImplicitThis\": true");
-            if (AlwaysStrict) stringBuilder.Append(",").AppendLine("    \"alwaysStrict\": true");
-            if (NoUnusedLocals) stringBuilder.Append(",").AppendLine("    \"noUnusedLocals\": true");
-            if (NoUnusedParameters) stringBuilder.Append(",").AppendLine("    \"noUnusedParameters\": true");
-            if (NoImplicitReturns) stringBuilder.Append(",").AppendLine("    \"noImplicitReturns\": true");
-            if (NoFallthroughCasesInSwitch) stringBuilder.Append(",").AppendLine("    \"noFallthroughCasesInSwitch\": true");
-            if (ModuleResolution != null) stringBuilder.Append(",").AppendLine($"    \"moduleResolution\": \"{ModuleResolution.GetStringValue()}\"");
-            if (!string.IsNullOrEmpty(BaseUrl)) stringBuilder.Append(",").AppendLine($"    \"baseUrl\": \"{BaseUrl}\"");
-            if (Any(Paths))
-            {
-                stringBuilder.Append(",").AppendLine("    \"paths\": {");
-                var count = Paths.Count();
-                for (int i = 0; i < count; i++)
-                {
-                    var pathEntry = Paths.ElementAt(i);
-                    var paths = string.Join(", ", pathEntry.Value);
-                    stringBuilder.AppendLine($"      \"{pathEntry.Key}\": [{paths}]");
-                    if (i != count - 1) stringBuilder.Append(",");
-                }
-                stringBuilder.AppendLine("    }");
-            }
-            if (Any(RootDirs))
-            {
-                stringBuilder.Append(",").AppendLine("    \"rootDirs\": [");
-                var count = RootDirs.Count();
-                for (int i = 0; i < count; i++)
-                {
-                    stringBuilder.AppendLine($"      \"{RootDirs.ElementAt(i)}\"");
-                    if (i != count - 1) stringBuilder.Append(",");
-                }
-                stringBuilder.AppendLine("    ]");
-            }
-            if (Any(TypeRoots))
-            {
-                stringBuilder.Append(",").AppendLine("    \"typeRoots\": [");
-                var count = TypeRoots.Count();
-                for (int i = 0; i < count; i++)
-                {
-                    stringBuilder.AppendLine($"      \"{TypeRoots.ElementAt(i)}\"");
-                    if (i != count - 1) stringBuilder.Append(",");
-                }
-                stringBuilder.AppendLine("    ]");
-            }
-            if (Any(Types))
-            {
-                stringBuilder.Append(",").AppendLine("    \"types\": [");
-                var count = Types.Count();
-                for (int i = 0; i < count; i++)
-                {
-                    stringBuilder.AppendLine($"      \"{Types.ElementAt(i)}\"");
-                    if (i != count - 1) stringBuilder.Append(",");
-                }
-                stringBuilder.AppendLine("    ]");
-            }
-            if (AllowSyntheticDefaultImports) stringBuilder.Append(",").AppendLine("    \"allowSyntheticDefaultImports\": true");
-            if (EsModuleInterop) stringBuilder.Append(",").AppendLine("    \"esModuleInterop\": true");
-            if (PreserveSymlinks) stringBuilder.Append(",").AppendLine("    \"preserveSymlinks\": true");
-            if (!string.IsNullOrEmpty(SourceRoot)) stringBuilder.Append(",").AppendLine($"    \"sourceRoot\": \"{SourceRoot}\"");
-            if (!string.IsNullOrEmpty(MapRoot)) stringBuilder.Append(",").AppendLine($"    \"mapRoot\": \"{MapRoot}\"");
-            if (InlineSourceMap) stringBuilder.Append(",").AppendLine("    \"inlineSourceMap\": true");
-            if (InlineSources) stringBuilder.Append(",").AppendLine("    \"inlineSources\": true");
-            if (ExperimentalDecorators) stringBuilder.Append(",").AppendLine("    \"experimentalDecorators\": true");
-            if (EmitDecoratorMetadata) stringBuilder.Append(",").AppendLine("    \"emitDecoratorMetadata\": true");
-            if (Any(IncludePaths))
-            {
-                stringBuilder.AppendLine("  },").AppendLine("  \"include\": [");
-                var count = IncludePaths.Count();
-                for (int i = 0; i < count; i++)
-                {
-                    stringBuilder.AppendLine($"    \"{IncludePaths.ElementAt(i)}\"");
-                    if (i != count - 1) stringBuilder.Append(",");
-                }
-                stringBuilder.AppendLine("    ]");
-            }
-            return stringBuilder
-                .AppendLine("  }")
-                .AppendLine("}")
+            => new StringBuilder()
+                .Append(OpenCurlyBrace)
+                .AppendIndentedLine()
+                .AppendProperty(CompilerOptions)
+                .Append(OpenCurlyBrace)
+                .AppendStringProperty(nameof(Target), Target.GetStringValue(), IndentTwo, appendComma: false)
+                .AppendStringProperty(nameof(Module), Module.GetStringValue(), IndentTwo)
+                .ConditionallyAppendArray(nameof(Lib), Lib.Select(lib => lib.GetStringValue()), IndentTwo)
+                .ConditionallyAppendBool(nameof(AllowJs), AllowJs, IndentTwo)
+                .ConditionallyAppendBool(nameof(CheckJs), CheckJs, IndentTwo)
+                .ConditionallyAppendString(nameof(Jsx), Jsx.GetStringValue(), IndentTwo)
+                .ConditionallyAppendBool(nameof(Declaration), Declaration, IndentTwo)
+                .ConditionallyAppendBool(nameof(SourceMap), SourceMap, IndentTwo)
+                .ConditionallyAppendString(nameof(OutFile), OutFile, IndentTwo)
+                .ConditionallyAppendString(nameof(OutDir), OutDir, IndentTwo)
+                .ConditionallyAppendString(nameof(RootDir), RootDir, IndentTwo)
+                .ConditionallyAppendBool(nameof(RemoveComments), RemoveComments, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoEmit), NoEmit, IndentTwo)
+                .ConditionallyAppendBool(nameof(ImportHelpers), ImportHelpers, IndentTwo)
+                .ConditionallyAppendBool(nameof(DownLevelIteration), DownLevelIteration, IndentTwo)
+                .ConditionallyAppendBool(nameof(IsolatedModules), IsolatedModules, IndentTwo)
+                .ConditionallyAppendBool(nameof(Strict), Strict, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoImplicitAny), NoImplicitAny, IndentTwo)
+                .ConditionallyAppendBool(nameof(StrictNullChecks), StrictNullChecks, IndentTwo)
+                .ConditionallyAppendBool(nameof(StrictFunctionsTypes), StrictFunctionsTypes, IndentTwo)
+                .ConditionallyAppendBool(nameof(StrictPropertyInitialization), StrictPropertyInitialization, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoImplicitThis), NoImplicitThis, IndentTwo)
+                .ConditionallyAppendBool(nameof(AlwaysStrict), AlwaysStrict, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoUnusedLocals), NoUnusedLocals, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoUnusedParameters), NoUnusedParameters, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoImplicitReturns), NoImplicitReturns, IndentTwo)
+                .ConditionallyAppendBool(nameof(NoFallthroughCasesInSwitch), NoFallthroughCasesInSwitch, IndentTwo)
+                .ConditionallyAppendString(nameof(ModuleResolution), ModuleResolution?.GetStringValue(), IndentTwo)
+                .ConditionallyAppendString(nameof(BaseUrl), BaseUrl, IndentTwo)
+                .ConditionallyAppendObject(nameof(Paths), Paths, IndentTwo)
+                .ConditionallyAppendArray(nameof(RootDirs), RootDirs, IndentTwo)
+                .ConditionallyAppendArray(nameof(TypeRoots), TypeRoots, IndentTwo)
+                .ConditionallyAppendArray(nameof(Types), Types, IndentTwo)
+                .ConditionallyAppendBool(nameof(AllowSyntheticDefaultImports), AllowSyntheticDefaultImports, IndentTwo)
+                .ConditionallyAppendBool(nameof(EsModuleInterop), EsModuleInterop, IndentTwo)
+                .ConditionallyAppendBool(nameof(PreserveSymlinks), PreserveSymlinks, IndentTwo)
+                .ConditionallyAppendString(nameof(SourceRoot), SourceRoot, IndentTwo)
+                .ConditionallyAppendString(nameof(MapRoot), MapRoot, IndentTwo)
+                .ConditionallyAppendBool(nameof(InlineSourceMap), InlineSourceMap, IndentTwo)
+                .ConditionallyAppendBool(nameof(InlineSources), InlineSources, IndentTwo)
+                .ConditionallyAppendBool(nameof(ExperimentalDecorators), ExperimentalDecorators, IndentTwo)
+                .ConditionallyAppendBool(nameof(EmitDecoratorMetadata), EmitDecoratorMetadata, IndentTwo)
+                .AppendLine(IndentOne, CloseCurlyBrace)
+                .ConditionallyAppendArray(nameof(Include), Include)
+                .AppendLine(CloseCurlyBrace)
                 .AppendLine()
                 .ToString();
-        }
     }
 }
