2761c3f4315baa87c6f2cb261b57c26e9b2fc01f
Christopher Dorst <chris@dorst.email>
2018-04-11 02:28:10 -0500

    Add EnumFlagPositionConstants

---
+++ DevOps.Primitives.TypeScript/EnumFlagPositionConstants.cs
@@ -0,0 +1,37 @@
+﻿namespace DevOps.Primitives.TypeScript
+{
+    public static class EnumFlagPositionConstants
+    {
+        public const byte _0 = 1 << 0;
+        public const byte _1 = 1 << 1;
+        public const byte _2 = 1 << 2;
+        public const byte _3 = 1 << 3;
+        public const byte _4 = 1 << 4;
+        public const byte _5 = 1 << 5;
+        public const byte _6 = 1 << 6;
+        public const byte _7 = 1 << 7;
+        public const short _8 = 1 << 8;
+        public const short _9 = 1 << 9;
+        public const short _10 = 1 << 10;
+        public const short _11 = 1 << 11;
+        public const short _12 = 1 << 12;
+        public const short _13 = 1 << 13;
+        public const short _14 = 1 << 14;
+        public const int _15 = 1 << 15;
+        public const int _16 = 1 << 16;
+        public const int _17 = 1 << 17;
+        public const int _18 = 1 << 18;
+        public const int _19 = 1 << 19;
+        public const int _20 = 1 << 20;
+        public const int _21 = 1 << 21;
+        public const int _22 = 1 << 22;
+        public const int _23 = 1 << 23;
+        public const int _24 = 1 << 24;
+        public const int _25 = 1 << 25;
+        public const int _26 = 1 << 26;
+        public const int _27 = 1 << 27;
+        public const int _28 = 1 << 28;
+        public const int _29 = 1 << 29;
+        public const int _30 = 1 << 30;
+    }
+}
