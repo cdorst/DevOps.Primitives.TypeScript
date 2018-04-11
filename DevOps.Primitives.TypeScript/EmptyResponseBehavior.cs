db841766fa4a0423978703c365dff469475ffb89
Christopher Dorst <chris@dorst.email>
2018-04-11 02:28:41 -0500

    Add EmptyResponseBehavior

---
+++ DevOps.Primitives.TypeScript/EmptyResponseBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static DevOps.Primitives.TypeScript.EnumFlagPositionConstants;
+
+namespace DevOps.Primitives.TypeScript
+{
+    /// <summary>Describes behavior taken when responding with no data</summary>
+    [Flags]
+    public enum EmptyResponseBehavior : byte
+    {
+        None,
+        ExcludeEmptyProperties  = _0,
+        ReturnEmptyArray        = _1,
+        ReturnEmptyObject       = _2,
+        ReturnEmptyString       = _3,
+        ReturnNull              = _4,
+        Default = ExcludeEmptyProperties | ReturnNull
+    }
+}
